24b15842049cf943c3464068a0cdbb2ba3801f69
Jana Sperschneider <jana.sperschneider@csiro.au>
2026-05-13 16:06:07 +1000

    Update clean_gff3.py

--- clean_gff3.py
+++ clean_gff3.py
@@ -3,10 +3,10 @@
 """
 EffectorGeneP: gene annotation in pathogen genomes
 
-Copyright (c) 2025 Commonwealth Scientific and Industrial Research Organisation (CSIRO) ABN 41 687 119 230.
+Copyright (c) 2026 Commonwealth Scientific and Industrial Research Organisation (CSIRO) ABN 41 687 119 230.
 
 This work is freely available for non-commercial scientific research, non-commercial education, 
-or non-commercial research projects, under the CSIRO Non-Commercial License (https://github.com/JanaSperschneider/EffectorGeneP/blob/main/LICENCE). 
+or non-commercial research projects, under the CSIRO Software License Agreement (https://github.com/JanaSperschneider/EffectorGeneP/blob/main/LICENCE). 
 """
 # -----------------------------------------------------------------------------------------------------------
 import os
